89c647e348647dc52c4f92996f27394c2925a000
Russell Stroebel <russell@manifoldsa.onmicrosoft.com>
2024-09-19 17:03:38 +0200

    Add project files.

---
+++ AirTimeCityJrnlManager/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace AirTimeCityJrnlManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtLicense = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSerial = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDataPath = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.GetInventoryItem = new System.Windows.Forms.Button();
+            this.txtItemCode = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCustCode = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(272, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Test SDK Connection";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtLicense
+            // 
+            this.txtLicense.Location = new System.Drawing.Point(211, 70);
+            this.txtLicense.Name = "txtLicense";
+            this.txtLicense.Size = new System.Drawing.Size(128, 20);
+            this.txtLicense.TabIndex = 1;
+            this.txtLicense.Text = "DK198110007";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "License";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Serial No";
+            // 
+            // txtSerial
+            // 
+            this.txtSerial.Location = new System.Drawing.Point(211, 96);
+            this.txtSerial.Name = "txtSerial";
+            this.txtSerial.Size = new System.Drawing.Size(128, 20);
+            this.txtSerial.TabIndex = 3;
+            this.txtSerial.Text = "5635796";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(148, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Data Path";
+            // 
+            // txtDataPath
+            // 
+            this.txtDataPath.Location = new System.Drawing.Point(211, 122);
+            this.txtDataPath.Name = "txtDataPath";
+            this.txtDataPath.Size = new System.Drawing.Size(272, 20);
+            this.txtDataPath.TabIndex = 5;
+            this.txtDataPath.Text = "C:\\Pastel19\\_Demo";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(79, 434);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(656, 137);
+            this.txtResult.TabIndex = 7;
+            // 
+            // GetInventoryItem
+            // 
+            this.GetInventoryItem.Location = new System.Drawing.Point(396, 275);
+            this.GetInventoryItem.Name = "GetInventoryItem";
+            this.GetInventoryItem.Size = new System.Drawing.Size(132, 28);
+            this.GetInventoryItem.TabIndex = 8;
+            this.GetInventoryItem.Text = "Get Inventory Item";
+            this.GetInventoryItem.UseVisualStyleBackColor = true;
+            this.GetInventoryItem.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtItemCode
+            // 
+            this.txtItemCode.Location = new System.Drawing.Point(267, 280);
+            this.txtItemCode.Name = "txtItemCode";
+            this.txtItemCode.Size = new System.Drawing.Size(128, 20);
+            this.txtItemCode.TabIndex = 10;
+            this.txtItemCode.Text = "ACC/LOC";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Enter Known Item Code";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(275, 418);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(276, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Results from Get Item and Get Customer will appear here.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(76, 318);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(186, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Enter Known Customer Account Code";
+            // 
+            // txtCustCode
+            // 
+            this.txtCustCode.Location = new System.Drawing.Point(267, 315);
+            this.txtCustCode.Name = "txtCustCode";
+            this.txtCustCode.Size = new System.Drawing.Size(128, 20);
+            this.txtCustCode.TabIndex = 14;
+            this.txtCustCode.Text = "ACK001";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(396, 310);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 28);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Get Customer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(534, 318);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "(SDK.GetRecord)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(534, 283);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(92, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "(SDK.GetNearest)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(56, 38);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(718, 16);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Enter relevant details here and click \"Test SDK Connection\". This is the fist bas" +
+    "ic test that the app can connect to the SDK.";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(114, 233);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(523, 16);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Enter different values in the 2 textboxes here to test the SDK connection to the " +
+    "data itself. ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(396, 344);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 28);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Create Sales Order";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 652);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtCustCode);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtItemCode);
+            this.Controls.Add(this.GetInventoryItem);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDataPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSerial);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLicense);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLicense;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSerial;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDataPath;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button GetInventoryItem;
+        private System.Windows.Forms.TextBox txtItemCode;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCustCode;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ AirTimeCityJrnlManager/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows.Forms;
+using PasSDK;
+namespace AirTimeCityJrnlManager
+{
+    public partial class Form1 : Form
+    {
+        public static PastelPartnerSDK SDK = new PastelPartnerSDK();
+        string itemline = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string Authenticate()
+        {
+            SDK.SetLicense(txtLicense.Text, txtSerial.Text);
+            string strReturn = SDK.SetDataPath(txtDataPath.Text);
+            return strReturn;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SDK.SetLicense(txtLicense.Text, txtSerial.Text);
+            string strReturn = SDK.SetDataPath(txtDataPath.Text);
+            if (strReturn.StartsWith("0"))
+            {
+                MessageBox.Show("Yay, Connected");
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Err - " + strReturn);
+                return;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtItemCode.Text.Length > 0)
+            {
+                Authenticate();
+                string itemcode = txtItemCode.Text.Trim();
+                string argFileName = "ACCPRD";
+                short argKeyNumber = 0;
+                itemline = SDK.GetNearest(ref argFileName, ref argKeyNumber, ref itemcode);
+                txtResult.Text = itemline;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (txtCustCode.Text.Length > 0)
+            {
+                Authenticate();
+                string AccCode = txtCustCode.Text.Trim().PadRight(6);
+                string argFileName = "ACCMASD";
+                short argKeyNumber = 0;
+                itemline = SDK.GetRecord(ref argFileName, ref argKeyNumber, ref AccCode);
+                txtResult.Text = itemline;
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string StrReturn = string.Empty;
+            StrReturn = Authenticate();
+
+            if (StrReturn.StartsWith("0"))
+            {
+                SDK.OpenDocumentFiles();
+            } else
+            {
+                MessageBox.Show("Error 1 " + StrReturn);
+               return;
+            }
+                StrReturn = SDK.DefineDocumentHeader("||Y|ACK001|05/08/2024||N|0|Message no.1|Message no.2|Message no.3|Delivery no.1|Delivery no.2|Delivery no.3|Delivery no.4|Delivery no.5||00||05/03/1999|011-7402156|Johnny|011-7402157|1");
+            
+                if (StrReturn.StartsWith("0"))
+                {
+                    StrReturn = SDK.DefineDocumentLine("0|1|1043|667||15|||ACC/LOC|Toc Deep Sleep|4|JHB|");
+                }
+            else
+            {
+                MessageBox.Show("Error 2 " + StrReturn);
+                return;
+            }
+            StrReturn = SDK.ImportDocument(102);
+            MessageBox.Show(StrReturn);
+            SDK.CloseDocumentFiles();
+        }
+    }
+}
